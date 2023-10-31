6daccc59a058223ec5e2e003de4185e86846cbf0
JorgeMartinez16 <jorgemariomartinez16@gmail.com>
2023-10-31 17:50:10 -0500

    agregando Expected & wait to user and password

--- src/test/java/utils/pages/LoginPage.java
+++ src/test/java/utils/pages/LoginPage.java
@@ -3,24 +3,29 @@ package utils.pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class LoginPage {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10)); // Establece un tiempo máximo de espera de 10 segundos
     }
-
     public WebElement getUsernameField() {
-        return driver.findElement(By.id("user-name"));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("user-name")));
     }
 
     public WebElement getPasswordField() {
-        return driver.findElement(By.id("password"));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("password")));
     }
 
     public WebElement getLoginButton() {
-        return driver.findElement(By.id("login-button"));
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("login-button")));
+
     }
 
 
